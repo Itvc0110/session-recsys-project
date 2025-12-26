3b7ce43696df16e75fb2bee80551c9874f0b086c
Itvc0110 <162940999+Itvc0110@users.noreply.github.com>
2025-12-27 05:06:51 +0700

    Update gru4rec.py

---
+++ models/gru4rec.py
@@ -0,0 +1,58 @@
+import torch
+import torch.nn as nn
+from models.base_model import BaseSequentialModel
+
+class GRU4Rec(BaseSequentialModel):
+    def __init__(self, config):
+        super().__init__(config)
+        self.hidden_size = config['hidden_size']
+
+        self.item_embedding = nn.Embedding(self.num_items, self.embedding_size, padding_idx=0)
+        self.emb_dropout = nn.Dropout(config['dropout_prob'])
+        self.gru_layers = nn.GRU(
+            input_size=self.embedding_size, hidden_size=self.hidden_size,
+            num_layers=config['num_layers'], bias=False, batch_first=True
+        )
+        self.dense = nn.Linear(self.hidden_size, self.embedding_size)
+
+        self.apply(self._init_weights)
+
+    def _init_weights(self, module):
+        if isinstance(module, nn.Embedding):
+            nn.init.xavier_normal_(module.weight)
+        elif isinstance(module, nn.GRU):
+            nn.init.xavier_uniform_(module.weight_hh_l0)
+            nn.init.xavier_uniform_(module.weight_ih_l0)
+
+    def gather_indexes(self, output, gather_index):
+        gather_index = gather_index.view(-1, 1, 1).expand(-1, -1, output.shape[-1])
+        return output.gather(dim=1, index=gather_index).squeeze(1)
+
+    def forward(self, item_seq, item_seq_len):
+        item_seq_emb = self.item_embedding(item_seq)
+        item_seq_emb_dropout = self.emb_dropout(item_seq_emb)
+        gru_output, _ = self.gru_layers(item_seq_emb_dropout)
+        gru_output = self.dense(gru_output)
+        return self.gather_indexes(gru_output, item_seq_len - 1)
+
+    def calculate_loss(self, interaction):
+        item_seq = interaction['item_seq']
+        item_seq_len = interaction['item_seq_len']
+        pos_items = interaction['pos_item'][:, -1]
+        neg_items = interaction['neg_item'][:, -1]
+        seq_output = self.forward(item_seq, item_seq_len)
+
+        pos_emb = self.item_embedding(pos_items)
+        neg_emb = self.item_embedding(neg_items)
+        pos_score = (seq_output * pos_emb).sum(-1)
+        neg_score = (seq_output * neg_emb).sum(-1)
+        loss = -torch.log(torch.sigmoid(pos_score - neg_score)).mean()
+        return loss
+
+    def full_sort_predict(self, interaction):
+        item_seq = interaction['item_seq']
+        item_seq_len = interaction['item_seq_len']
+        seq_output = self.forward(item_seq, item_seq_len)
+        test_items_emb = self.item_embedding.weight
+        scores = torch.matmul(seq_output, test_items_emb.transpose(0, 1))
+        return scores
